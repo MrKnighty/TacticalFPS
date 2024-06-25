071f4845aa5c10a29ad1985ebb06bafd97b3d498
MrKnighty <dooty2nd@hotmail.com>
2024-06-25 13:23:46 +0930

    Forgot to commit- was emergency build patches

--- Assets/ExitGame.cs
+++ Assets/ExitGame.cs
@@ -5,6 +5,7 @@ public class ExitGame : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if(other.transform.tag == "Player")
         UnityEngine.SceneManagement.SceneManager.LoadScene(2);
     }
     public void Exit()
