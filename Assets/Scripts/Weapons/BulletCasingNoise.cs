cd59a9ed9bc418818637363cd312325c13a2a230
MrKnighty <dooty2nd@hotmail.com>
2024-07-18 23:36:00 +0930

    fixed anims of sniper, sped up all ADS and equip\dequip animations. fixed bullet casing making too much noise. made them bouncier. they now fire out of guns correctly. different sizes ones depending on gun

--- Assets/Scripts/Weapons/BulletCasingNoise.cs
+++ Assets/Scripts/Weapons/BulletCasingNoise.cs
@@ -4,10 +4,21 @@ public class BulletCasingNoise : MonoBehaviour
 {
     [SerializeField] AudioClip[] inpactSounds;
     [SerializeField] AudioSource source;
+    bool canMakeSound = true;
 
     private void OnCollisionEnter(Collision collision)
     {
-     
-       source.PlayOneShot(inpactSounds[Random.Range(0, inpactSounds.Length - 1)], GameControllsManager.audioVolume * 0.7f);
+        if (!canMakeSound || collision.gameObject.layer == 9)//9 is the layer of the casing
+            return;
+    
+        canMakeSound = false;
+        source.PlayOneShot(inpactSounds[Random.Range(0, inpactSounds.Length - 1)], GameControllsManager.audioVolume * 0.7f);
+        Invoke("TimerOver", 0.25f);
+    }
+
+
+    void TimerOver()
+    {
+        canMakeSound = true;
     }
 }
