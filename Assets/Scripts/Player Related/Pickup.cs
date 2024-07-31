7e73cf56c9fb4a8e3fde0c7be5a27fa64c4d3fca
MrKnighty <dooty2nd@hotmail.com>
2024-07-31 22:45:23 +0930

    updated pickup with new models. made prefabs for all new models, updates to level 2, adding containment room

--- Assets/Scripts/Player Related/Pickup.cs
+++ Assets/Scripts/Player Related/Pickup.cs
@@ -6,9 +6,44 @@ public class Pickup : Interactable
     [SerializeField] PickupType type;
     [SerializeField] int amount;
 
+    [SerializeField] GameObject pistol, rifle, smg, shotgun,health;
     protected override void TriggerEvent()
     {
         GunManager.instance.GiveAmmo(type, amount);
         Destroy(gameObject);
+    }
+
+    private void OnValidate()
+    {
+        pistol.SetActive(false);
+        rifle.SetActive(false);
+        smg.SetActive(false);
+        shotgun.SetActive(false);
+        health.SetActive(false);
+
+        switch (type)
+        {
+            
+            case PickupType.PISTOL_AMMO:
+                pistol.SetActive(true);
+                break;
+            case PickupType.RIFLE_AMMO:
+                rifle.SetActive(true); 
+                break;
+            case PickupType.SHOTGUN_AMMO:
+                shotgun.SetActive(true); 
+                break;
+            case PickupType.SMG_AMMO:
+                smg.SetActive(true); 
+                break;
+            case PickupType.HEAL_SYRINGE:
+                health.SetActive(true); 
+                break;
+
+
+
+
+        }
+
     }
 }
